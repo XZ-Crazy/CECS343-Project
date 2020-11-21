82dc3838faf82c67e7fbe862cd76b94a3818a3fe
smlgrc <samuel.garcia01@student.csulb.edu>
2020-11-20 17:58:37 -0800

    Updated Customer and Salesperson classes

--- project/Customer.java
+++ project/Customer.java
@@ -1,5 +1,83 @@
 package project;
 
-public class Customer {
+public class Customer
+{
+    private String name;
+    private float taxPercentage;
+    private boolean delinquencyStatus;
+    
+    public Customer()
+    {
+        this.name = "";
+        this.taxPercentage = 0;
+        this.delinquencyStatus = false;
+    }
+    /*---Overloaded Constructors---*/
 
+    public Customer(String name, float taxPercentage, Boolean delinquencyStatus)
+    {
+        this.name = name;
+        this.taxPercentage = taxPercentage;
+        this.delinquencyStatus = delinquencyStatus;
+    }
+    /*---Accessors---*/
+    public String getName()
+    {
+        return this.name;
+    }
+    public float getTaxPercentage()
+    {
+        return this.taxPercentage;
+    }
+    public Boolean getDelinquencyStatus()
+    {
+        return this.delinquencyStatus;
+    }
+    /*---Mutators---*/
+    public void setDelinquencyStatus(Boolean status)
+    {
+        this.delinquencyStatus = status;
+    }
+
+    /**
+     * Returns a string in a specific format.
+     */
+    @Override
+    public String toString()
+    {
+        return "Customer Name: " + name + ", Tax Percentage: " + taxPercentage + ", Delinquency Status: " + delinquencyStatus;
+    }   
+    /**
+     * maybe we can use to check if we already have a customer in the system before
+     * registering a new one
+     */
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o instanceof Customer)
+        {
+            Customer c = (Customer) o;
+            if (name.equals(c.getName()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * Compares two objects, first by last name then by first to see if
+     * they are equal
+     * @param o - explicit object to the compared.
+     * @return 0 if equal, otherwise negative or positive depending on if
+     * the last name is bigger or smaller.
+
+    public int compareTo(Customer o)
+    {
+        if (lastName.compareTo(o.getLastName()) == 0)
+        {
+            return firstName.compareTo(o.getFirstName());
+        }
+        return lastName.compareTo(o.getLastName());
+    }
+    */
 }
--- project/Salesperson.java
+++ project/Salesperson.java
@@ -1,5 +1,89 @@
 package project;
 
-public class Salesperson {
+public class Salesperson
+{
+    private String name;
+    private float totalSales;
+    private float totalCommission;
+    private float commissionRate;
+    
+    public Salesperson()
+    {
+        this.name = "";
+        this.totalSales = 0;
+        this.totalCommission = 0;
+        this.commissionRate = 0;
+    }
+    /*---Overloaded Constructors---*/
 
+    public Salesperson(String name, float totalSales, float totalCommission, float commissionRate)
+    {
+        this.name = name;
+        this.totalSales = totalSales;
+        this.totalCommission = totalCommission;
+        this.commissionRate = commissionRate;
+    }
+    /*---Accessors---*/
+    public String getName()
+    {
+        return this.name;
+    }
+    public float getTotalSales()
+    {
+        return this.totalSales;
+    }
+    public float getTotalCommission()
+    {
+        return this.totalCommission;
+    }
+    public float getCommissionRate()
+    {
+        return this.commissionRate;
+    }
+    /*---Mutators---*/
+    public void setCommissionRate(float rate)
+    {
+        this.commissionRate = rate;
+    }
+
+    /**
+     * Returns a string in a specific format.
+     */
+    @Override
+    public String toString()
+    {
+        return "Salesperson Name: " + name + ", Total Sales: " + totalSales + ", Total Commission: " + totalCommission + ", Commission Rate: " + commissionRate;
+    }   
+
+    /**
+    @Override
+    public boolean equals(Object o)
+    {
+        if (o instanceof Salesperson)
+        {
+            Salesperson c = (Salesperson) o;
+            if (name.equals(c.getName()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    */
+    /**
+     * Compares two objects, first by last name then by first to see if
+     * they are equal
+     * @param o - explicit object to the compared.
+     * @return 0 if equal, otherwise negative or positive depending on if
+     * the last name is bigger or smaller.
+
+    public int compareTo(Salesperson o)
+    {
+        if (lastName.compareTo(o.getLastName()) == 0)
+        {
+            return firstName.compareTo(o.getFirstName());
+        }
+        return lastName.compareTo(o.getLastName());
+    }
+    */
 }
