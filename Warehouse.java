e369af81c5bf1e29e30c7325fbd59329024a9168
Steven Duran <stevenduran1@yahoo.com>
2020-11-29 21:03:23 -0800

    Added Warehouse class.

---
+++ Warehouse.java
@@ -0,0 +1,16 @@
+package project;
+
+/**
+ *  Warehouse class
+ * @author Steven Duran
+ * @version 1.0
+ * @since 2020-11-29
+ * CECS 343
+ *
+ */
+
+import java.util.*;
+
+public class Warehouse {
+
+}
