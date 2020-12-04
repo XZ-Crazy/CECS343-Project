034efb73d34f85502fd0d94523762079d749bb83
smlgrc <samuel.garcia01@student.csulb.edu>
2020-12-03 20:48:14 -0800

    Added Gson Examples. having issues with deserializing an arraylist of type customer.

--- project/Runner.java
+++ project/Runner.java
@@ -1,4 +1,3 @@
-/*
 package project;
 
 import java.util.ArrayList;
@@ -14,33 +13,33 @@ public class Runner
 {
 	public static void main(String[] args) throws JSONException 
 	{
-		// Creates new customer 
+		System.out.println("---Creates new customer---");
 		Customer Sam = new Customer("Samuel Winchester", (float) 0.1, false);
 		System.out.println("Customer created -> " + Sam);
 		
-		// Creates new salesperson/employee 
+		System.out.println("\n---Creates new salesperson/employee---"); 
 		Salesperson Dean = new Salesperson("Dean Winchester", 0, 0, (float) 0.3);
-		System.out.println("\nSalesperson created -> " + Dean);
+		System.out.println("Salesperson created -> " + Dean);
 		
-		// Sets delinquency status 
+	    System.out.println("\n---Sets delinquency status---"); 
 		Sam.setDelinquencyStatus(true);
-		System.out.println("\nCurrent delinquency status of " + Sam.getName() + " = " + Sam.getDelinquencyStatus());
+		System.out.println("Current delinquency status of " + Sam.getName() + " = " + Sam.getDelinquencyStatus());
 		
-		// Creates current or customer dates 
+		System.out.println("\n---Creates current or customer dates---"); 
 		Date date1 = new Date();
-		System.out.println("\nCurrent date is " + date1);
+		System.out.println("Current date is " + date1);
 		
 		Calendar date2 = Calendar.getInstance();
-		System.out.println("\nCurrent date is " + date2.getTime());
+		System.out.println("Current date is " + date2.getTime());
 		
 		Calendar date3 = Calendar.getInstance();
 		date3.set(1969, 4, 20);
-		System.out.println("\ndate3 date is " + date3.getTime());
+		System.out.println("date3 date is " + date3.getTime());
 		
-		// Compares two different dates 
+		System.out.println("\n---Compares two different dates---"); 
 		if (date2.after(date3) == true) // checks to see if date2 comes after date3
 		{
-			System.out.println("\nDate2 = " + date2.getTime());
+			System.out.println("Date2 = " + date2.getTime());
 			System.out.println("Date3 = " + date3.getTime());
 			System.out.println("Date2 is more in the future than Date3");
 		}
@@ -48,26 +47,58 @@ public class Runner
 		{
 			System.out.println("\nDate3 = " + date3.getTime());
 			System.out.println("Date2 = " + date2.getTime());
-			System.out.println("Date3 is more in the past than Date2");
+			System.out.println("Date3 is more in the past than Date2\n");
 		}
 		
-		Gson gson = new Gson();
+		System.out.println("---Create a JSON string---"); 
+		Customer c1 = new Customer();
+		c1.setName("Sam");
 		
-		// Create a JSON file 
+		Gson json = new Gson();
 		
-		ArrayList<Student> students = new ArrayList<Student>();
-        students.add(new Student("Tom",3.921));
-        students.add(new Student("Dave",4.0));
-        students.add(new Student("Bill",2.0));
-        
-        
+		String response = json.toJson(c1); // converts c1 object to json string
+		System.out.println(response);
 		
-        JSONObject database = new JSONObject();
-		database.put("firstname", "Jason");
-		database.put("lastname", "Cox");
-		database.put("website", "www.jsonc.com");
+		Customer c2 = json.fromJson(response, Customer.class); // takes in string version of a customer object and assigns it to an object in java
+		System.out.println(c2 + "\n"); // prints c2.toString()
+		
+		
+		
+		System.out.println("---Creating JSON files---");
+		ArrayList <Customer> customerList = new ArrayList<>();
+		customerList.add(new Customer("Sam", (float) 0.0, false));
+		customerList.add(new Customer("Steven", (float) 0.0, false));
+		customerList.add(new Customer("Alex", (float) 0.0, false));
+		customerList.add(new Customer("Anthony", (float) 0.0, false));
+		
+		Gson customerJson = new Gson();
+		
+		String customerJsonString = json.toJson(customerList);
+		System.out.println(customerJsonString);
+		
+		ArrayList <Customer> readCustomerList = json.fromJson(customerJsonString, ArrayList.class);
+		
+		System.out.println(readCustomerList.get(0)); // this is not returning a customer
+		
+		
+		// save a hash map of the inventory and the intergers that go with it
+		// inventory stored in hashmap to be put into jsonobject
+		
+		
+
 		
 	}
 
 }
-*/
+
+//ArrayList<Student> students = new ArrayList<Student>();
+//students.add(new Student("Tom",3.921));
+//students.add(new Student("Dave",4.0));
+//students.add(new Student("Bill",2.0));
+//
+//
+//
+//JSONObject database = new JSONObject();
+//database.put("firstname", "Jason");
+//database.put("lastname", "Cox");
+//database.put("website", "www.jsonc.com");
