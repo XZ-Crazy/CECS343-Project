ba469591c9d4956416220c707219c26aa56522f6
Steven Duran <stevenduran1@yahoo.com>
2020-12-06 04:52:47 -0800

    modified addStock(); took a lot of critical thinking. implemented try-catch to prevent NullPointerException in case user wants to get a product that isn't in the inventory

--- project/Warehouse.java
+++ project/Warehouse.java
@@ -53,9 +53,33 @@ public class Warehouse {
 		return inventory;
 	}
 	
+	/**
+	 * If the user wants to add a product that's already in stock (or increase the quantity)
+	 * the function will check if a product is already in stock and if so, quantity of said 
+	 * product is incremented. But if the user wants to add a new product, they can still do so.
+	 * 
+	 * @param product - product in the inventory
+	 * @param quantityToAdd - quantity added to the original quantity present in the inventory
+	 */
 	public void addStock(Product product, int quantityToAdd) {
 		
-		inventory.put(product, quantityToAdd);
+		try {
+			// is a particular product already in stock? if so, increment quantity of said product
+			if(inventory.containsKey(product) && inventory.get(product) > 0) {
+				
+				// inventory.get(product) returns the value/quantity of the product
+				// quantity to add is added to the quantity already present 
+				inventory.put(product, inventory.get(product) + quantityToAdd);
+			}
+			else if(inventory.isEmpty() || inventory.get(product) == 0) { // this statement is ran when adding a product that isn't already in stock
+				inventory.put(product, quantityToAdd);
+			}
+			else { // for all other cases
+				inventory.put(product, quantityToAdd);
+			}
+		} catch(NullPointerException e) {
+			System.out.println("There is(are) no product(s) available!");
+		}
 	}
 	
 	public void removeStock(Product product, int quantityToRemove) {
