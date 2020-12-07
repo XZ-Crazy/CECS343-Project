d43ffc388c786171e4b3e323b6567cb1c291280d
smlgrc <samuel.garcia01@student.csulb.edu>
2020-12-06 22:52:15 -0800

    Created InvoiceController class

---
+++ project/InvoiceController.java
@@ -0,0 +1,41 @@
+package project;
+
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+public class InvoiceController 
+{
+	//private static ArrayList<Warehouse> warehouses;
+	//private static ArrayList<Product> products;
+	
+	public static void initialize()
+	{
+		// create empty json file
+	}
+	
+	public static void deserializeJSON()
+	{
+		// read in json file and add warehouse and product objects to their arraylists
+	}
+	
+	public static void serializeJSON()
+	{
+		// 
+	}
+}
