2554c0e6054a51da64d372afb33fad68d6a8f1bf
Steven Duran <stevenduran1@yahoo.com>
2020-12-08 21:50:49 -0800

    Modified addStock method, added checkInStock, printTotalStock, printLowStock; still needs testing

--- project/WarehouseController.java
+++ project/WarehouseController.java
@@ -1,9 +1,6 @@
 package project;
 
 import java.util.*;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -187,7 +184,7 @@ public class WarehouseController
 		wObj = new Warehouse(inventory, name, address, city, state, zipCode, phoneNumber, warehouseID);
 		for(int i = 0; i < warehouses.size(); i++) {
 			if(warehouses.get(i).equals(wObj))
-				return false;
+				return true;
 		}
 		warehouses.add(wObj);
 		return true;
@@ -218,30 +215,91 @@ public class WarehouseController
 	 */
 	public static void addStock(Warehouse warehouse, HashMap<Product, Integer> inventory) {
 		
-		//pObj = (Product) inventory.keySet();
-		
-		// value of the inventory
-		//int quantityToAdd = inventory.get(pObj);
-		/*
-		 * Add product and quantity to inventory to have at least one product in the warehouse.
-		 * This eliminates the error-condition of the database not containing at least one product
-		 * before adding any product (use case #4)
-		 */
-		inventory.put(pObj, quantityToAdd);
-		
-		/*
-		 * For all warehouses, call addStock() from Warehouse class to add Product and Quantity.
-		 * Then compare if the elements in warehouses arrayLists are equal to the instances of Warehouse Entity Object
-		 * Finally, add the instance of Warehouse to the warehouses ArrayList
-		 */
-		for(int i = 0; i < warehouses.size(); i++) {
-//			if(inventory.containsKey(pObj)) {
-//				warehouse.addStock(pObj, quantityToAdd);
-//			}
-			//inventory.put(pObj, quantityToAdd);
-			warehouse.addStock(pObj, quantityToAdd);
-			warehouses.get(i).equals(warehouse);
+		for(Map.Entry<Product, Integer> products : inventory.entrySet()) {
+			
+			Product product = products.getKey();
+			Integer quantity = products.getValue();
+			warehouse.addStock(product, quantity);
+			
+			if(inventory.isEmpty() || inventory.get(product) == null) {
+				warehouse.addStock(product, quantity);
+			}
 		}
 		warehouses.add(warehouse);
+	}
+	
+	/**
+	 * Checks if there's any stock in the warehouse
+	 * @param inventory
+	 * @return boolean true
+	 */
+	public static boolean checkInStock(HashMap<Product, Integer> inventory) {
+		
+		if(inventory.containsKey(pObj) && inventory.get(pObj) > 0) {
+			return true;
+		}
+		else if(inventory.isEmpty() || inventory.keySet() == null || inventory.entrySet() == null) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static String printTotalStock() {
+		
+		//ArrayList<Product> sortedProducts = new ArrayList<Product>(inventory.keySet());
+		
+		HashMap<Product, Integer> inventory = new HashMap<Product, Integer>();
+		String totalStock = "";
+		
+		// Creating a list from elements of inventory hashmap
+		List<Map.Entry<Product, Integer>> sortedProducts = new ArrayList<Map.Entry<Product, Integer>>(inventory.entrySet());
+		
+		// Sort list in ascending order with Java Collections Framework and Comparator interface
+		Collections.sort(sortedProducts, new Comparator<Map.Entry<Product, Integer>>() {
+			@Override
+			public int compare(Map.Entry<Product, Integer> o1, Map.Entry<Product, Integer> o2) {
+				   return o1.getValue().compareTo(o2.getValue());
+			}
+		});
+		
+		// Put data from sorted list to inventory hashmap using for each loop
+		for(Map.Entry<Product, Integer> prod : sortedProducts) {
+			inventory.put(prod.getKey(), prod.getValue());
+//			System.out.println("Products: " + prod.getKey() + ", Quantity: " + prod.getValue());
+			totalStock = "Products: " + prod.getKey() + ", Quantity: " + prod.getValue();
+		}
+		return totalStock;
+		
+		//return new JSONObject(inventory).toString();
+		
+//		for(Product totalStock : sortedProducts)
+//			System.out.println("Products: " + totalStock + ", Quantity: " + inventory.get(totalStock));
+	}
+	
+	public static String printLowStock() {
+		
+		HashMap<Product, Integer> inventory = new HashMap<Product, Integer>();
+		String totalLowStock = "";
+		
+		if(inventory.get(pObj) <= 5) {
+			
+			// Creating a list from elements of inventory hashmap
+			List<Map.Entry<Product, Integer>> list = new ArrayList<Map.Entry<Product, Integer>>(inventory.entrySet());
+						
+			// Sort list in ascending order with Java Collections Framework and Comparator interface
+			Collections.sort(list, new Comparator<Map.Entry<Product, Integer>>() {
+				@Override
+				public int compare(Map.Entry<Product, Integer> o1, Map.Entry<Product, Integer> o2) {
+					return o1.getValue().compareTo(o2.getValue());
+				}
+			});
+			// Put data from sorted list to inventory hashmap using for each loop
+			for(Map.Entry<Product, Integer> prod : list) {
+				inventory.put(prod.getKey(), prod.getValue());
+				totalLowStock = "Products: " + prod.getKey() + ", Quantity: " + prod.getValue();
+			}
+		}
+		return totalLowStock;
+		//warehouses.add()
 	}
 }
