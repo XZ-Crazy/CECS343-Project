2ac9c00c1a8e804de1265751e45844cc7483a0cb
smlgrc <samuel.garcia01@student.csulb.edu>
2020-12-10 00:03:37 -0800

    testing controllers

--- project/Runner.java
+++ project/Runner.java
@@ -16,6 +16,11 @@ import org.json.simple.parser.ParseException;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
+import WarehouseApp.Customer;
+import WarehouseApp.Invoice;
+import WarehouseApp.Product;
+import WarehouseApp.Salesperson;
+import WarehouseApp.Warehouse;
 import project.InvoiceController;
 import project.WarehouseController;
 
@@ -166,8 +171,30 @@ public class Runner
 		
 		// Testing Warehouse and invoice Controllers
 		System.out.println("\n---Testing Warehouse and invoice Controllers---");
-		WarehouseController.initializeJSONFile();
-		InvoiceController.initializeJSONFile();
+		WarehouseController.initializeJSONFile(); // creates blank warehouses json file
+		InvoiceController.initializeJSONFile(); // creates blank invoices json file
+		
+		// For warehouse controller
+		HashMap<Product, Integer> inventory = new HashMap<>();
+		WarehouseController.addWarehouse(inventory, "Warehouse 1", "800 Socal Ave", "Los Angeles", "CA", "90035", "5623336425", "1");
+		
+		WarehouseController.serializeJSON();
+		
+		WarehouseController.deserializeJSONFile();
+		ArrayList<Warehouse> warehouseList = WarehouseController.getWarehouses();
+		System.out.println("Warehouse toString() = " + warehouseList.get(0));
+		
+		// for invoice controller
+		HashMap<Product, Integer> productList = new HashMap<>();
+		Customer c1 = new Customer("Sam", (float) 0.0, false);
+		Salesperson s1 = new Salesperson("Dean", (float) 0.0, (float) 0.0, (float) 0.0);
+		InvoiceController.addInvoice(c1, s1, productList, "123 Shit St", (float) 420.69);
+		
+		InvoiceController.serializeJSON();
+		
+		InvoiceController.deserializeJSONFile();
+		ArrayList<Invoice> invoiceList = InvoiceController.getInvoices();
+		System.out.println("Invoince toString() = " + invoiceList.get(0));
 		
 	}
 	
